90f0f841b12900173c8c91ab1a53fe3f3aba8d55
Karma-L-King <57346728+Karma-L-King@users.noreply.github.com>
2019-11-11 15:53:44 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var dt = new Date();
+document.getElementById("datetime").innerHTML = (("0" + dt.getDate()).slice(-2)) + "." + (("0" + (dt.getMonth() + 1)).slice(-2)) + "." + (dt.getFullYear()) + " " + (("0" + dt.getHours()).slice(-2)) + ":" + (("0" + dt.getMinutes()).slice(-2));
+``
+
+function clickCounter() {
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+        document.getElementById("Button1").style.backgroundColor = "green";
+    }
+}
